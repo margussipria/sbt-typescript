a3408e6f480148239bd3089dbd0827c38b346317
Margus Sipria <margus@sipria.fi>
2024-04-11 23:13:05 +0300

    Upgraded sbt to newest and allowing to add includeFilter and excludeFilter for sources.

--- src/main/scala/com/typesafe/sbt/typescript/SbtTypescript.scala
+++ src/main/scala/com/typesafe/sbt/typescript/SbtTypescript.scala
@@ -41,28 +41,32 @@ object SbtTypescript extends AutoPlugin {
 
   import Import.TypescriptKeys._
 
-  val typescriptUnscopedSettings = Seq(
+  private val typescriptUnscopedSettings: Seq[Def.Setting[_]] = Seq(
 
     includeFilter := GlobFilter("*.ts") | GlobFilter("*.tsx"),
 
     excludeFilter := GlobFilter("*.d.ts"),
 
-    sources := (sourceDirectory.value ** (includeFilter.value -- excludeFilter.value)).get,
+    sources := Def.task {
+      (sourceDirectory.value ** (includeFilter.value -- excludeFilter.value)).get
+    }.value,
 
-    jsOptions := JsObject(
-      "sourceRoot" -> JsString(sourceRoot.value),
-      "logLevel" -> JsString(logLevel.value.toString),
-      "rootDir" -> JsString(sourceDirectory.value.absolutePath),
-      "baseUrl" -> JsString((webJarsDirectory.value / "lib").absolutePath),
-      "configFiles" -> JsArray(configFiles.value.map(file => JsString(file)).toVector),
-      "projectBase" -> JsString(baseDirectory.value.absolutePath),
-      "sources" -> JsArray(
-        sources.value
-          .filter(_.isFile)
-          .map(file => JsString(file.absolutePath))
-          .toVector
-      )
-    ).toString()
+    jsOptions := Def.task {
+      JsObject(
+        "sourceRoot" -> JsString(sourceRoot.value),
+        "logLevel" -> JsString(logLevel.value.toString),
+        "rootDir" -> JsString(sourceDirectory.value.absolutePath),
+        "baseUrl" -> JsString((webJarsDirectory.value / "lib").absolutePath),
+        "configFiles" -> JsArray(configFiles.value.map(file => JsString(file)).toVector),
+        "projectBase" -> JsString(baseDirectory.value.absolutePath),
+        "sources" -> JsArray(
+          sources.value
+            .filter(_.isFile)
+            .map(file => JsString(file.absolutePath))
+            .toVector
+        )
+      ).toString()
+    }.value
   )
 
   def relative(base: String, fullPath: String): String = fullPath.replace(base, "")
@@ -86,13 +90,14 @@ object SbtTypescript extends AutoPlugin {
       TestAssets / configFiles := Vector.empty,
     ) ++ inTask(typescript)(
       SbtJsTask.jsTaskSpecificUnscopedProjectSettings ++
-        inConfig(Assets)(typescriptUnscopedSettings) ++
-        inConfig(TestAssets)(typescriptUnscopedSettings) ++
         Seq(
           Assets / taskMessage := "TypeScript compiling",
           TestAssets / taskMessage := "TypeScript test compiling"
         )
-    ) ++ SbtJsTask.addJsSourceFileTasks(typescript) ++ Seq(
+    ) ++ SbtJsTask.addJsSourceFileTasks(typescript) ++ inTask(typescript)(
+        inConfig(Assets)(typescriptUnscopedSettings) ++
+        inConfig(TestAssets)(typescriptUnscopedSettings)
+    ) ++ Seq(
       Assets / typescript := (Assets / typescript).dependsOn(Assets / webModules).value,
       TestAssets / typescript := (TestAssets / typescript).dependsOn(TestAssets / webModules).value,
     )
